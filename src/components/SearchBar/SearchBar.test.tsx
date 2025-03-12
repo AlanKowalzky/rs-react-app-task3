6e348b1f6d703e102a8e7f59a3382f6a53cbbca7
AlanKowalzky <alan.kowalzky@gmail.com>
2025-03-12 19:50:45 +0100

    refactor: test cov branch r-r-s

--- src/components/SearchBar/SearchBar.test.tsx
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, expect, test } from '@jest/globals';
-
+import '@testing-library/jest-dom'; 
 import SearchBar from './SearchBar';
 
 test('updates search value and triggers search', () => {
@@ -19,4 +19,27 @@ test('updates search value and triggers search', () => {
 
   // Sprawdź, czy funkcja onSearch została wywołana z prawidłową wartością
   expect(mockOnSearch).toHaveBeenCalledWith('pikachu');
+});
+test('calls onSearch when Enter key is pressed', () => {
+  const mockOnSearch = jest.fn();
+  render(<SearchBar onSearch={mockOnSearch} />);
+  
+  const input = screen.getByPlaceholderText('Search Pokemon');
+  fireEvent.change(input, { target: { value: 'pikachu' } });
+  fireEvent.keyDown(input, { key: 'Enter', code: 'Enter' });
+  
+  expect(mockOnSearch).toHaveBeenCalledWith('pikachu');
+  expect(input).toHaveValue('');
+});
+
+test('does not call onSearch when a different key is pressed', () => {
+  const mockOnSearch = jest.fn();
+  render(<SearchBar onSearch={mockOnSearch} />);
+  
+  const input = screen.getByPlaceholderText('Search Pokemon');
+  fireEvent.change(input, { target: { value: 'pikachu' } });
+  fireEvent.keyDown(input, { key: 'Tab', code: 'Tab' });
+  
+  expect(mockOnSearch).not.toHaveBeenCalled();
+  expect(input).toHaveValue('pikachu');
 });
